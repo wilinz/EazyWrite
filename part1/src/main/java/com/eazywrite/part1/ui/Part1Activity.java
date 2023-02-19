fd135e9a852217f4b71e9003b16042856d077769
jixiuy <2663465706@qq.com>
2023-02-19 14:35:20 +0800

    "把kotlin换成熟悉的java"

---
+++ part1/src/main/java/com/eazywrite/part1/ui/Part1Activity.java
@@ -0,0 +1,16 @@
+package com.eazywrite.part1.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.eazywrite.app.part1.R;
+
+public class Part1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part1_test);
+    }
+}
