5229c5d196f4128867f3090dbbd007aad1aaf688
jiangxiyu <2663465706@qq.com>
2023-03-01 09:07:51 +0800

    "忘记密码出现bug"

---
+++ app/src/main/java/com/eazywrite/app/data/model/ResetBean.java
@@ -0,0 +1,41 @@
+package com.eazywrite.app.data.model;
+
+public class ResetBean {
+
+    /**
+     * code
+     */
+    private String code;
+    /**
+     * username
+     */
+    private String username;
+    /**
+     * newPassword
+     */
+    private String newPassword;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
