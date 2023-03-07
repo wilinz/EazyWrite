f16749c531ebbe04e5e09729590f061c8cc99f4c
ToolsPeople <1813211082@qq.com>
2023-03-08 01:35:46 +0800

    "首页展示重构"

--- app/src/main/java/com/eazywrite/app/ui/bill/adapter/CallbackData.java
+++ app/src/main/java/com/eazywrite/app/ui/bill/adapter/CallbackData.java
@@ -1,8 +1,11 @@
 package com.eazywrite.app.ui.bill.adapter;
 
+import com.eazywrite.app.data.model.WeekBillBean;
 import com.eazywrite.app.ui.bill.fragment.InputViewModel;
+
+import java.util.List;
 
 public interface CallbackData {
 
-    void addData(InputViewModel viewModel);
+    void addData(List<WeekBillBean> weekBillBeanList);
 }
