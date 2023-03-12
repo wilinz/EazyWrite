b94f2645483f0454e27234ebb1375c6a10163b65
wilinz <weiizan71@gmail.com>
2023-03-12 19:23:45 +0800

    重构首页第2部分

--- app/src/main/java/com/eazywrite/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/eazywrite/app/ui/home/HomeFragment.kt
@@ -7,26 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material.icons.filled.More
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import com.eazywrite.app.R
 import com.eazywrite.app.ui.theme.EazyWriteTheme
 import com.google.android.material.datepicker.MaterialDatePicker
@@ -97,7 +100,7 @@ fun HomePage(paddingValues: PaddingValues = PaddingValues(0.dp)) {
                                 datePicker.show(context.supportFragmentManager, "date1")
                             }
                         },
-                        colors = ButtonDefaults.textButtonColors(contentColor = Color.White)
+                        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colorScheme.onPrimary)
                     ) {
                         Row(verticalAlignment = Alignment.CenterVertically) {
                             Text(text = dateTime, fontSize = 18.sp, fontWeight = FontWeight.Normal)
@@ -108,20 +111,92 @@ fun HomePage(paddingValues: PaddingValues = PaddingValues(0.dp)) {
             )
         },
         containerColor = Color.Transparent,
-    ) {
+    ) { paddingValues1 ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
         ) {
-            Box {
-                Image(
-                    painter = painterResource(id = R.drawable.bill_background),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop
-                )
-                Box(modifier = Modifier.padding(vertical = 32.dp, horizontal = 16.dp)) {
-
+            Column {
+                Box {
+                    Image(
+                        painter = painterResource(id = R.drawable.bill_background),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop
+                    )
+                    Box(modifier = Modifier.padding(top = paddingValues1.calculateTopPadding())) {
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            Column(
+                                modifier = Modifier.weight(1f),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(text = "月收入", color = MaterialTheme.colorScheme.onPrimary)
+                                Text(text = "￥5600", color = MaterialTheme.colorScheme.onPrimary)
+                            }
+                            Column(
+                                modifier = Modifier.weight(1f),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(text = "月支出", color = MaterialTheme.colorScheme.onPrimary)
+                                Text(text = "￥5600", color = MaterialTheme.colorScheme.onPrimary)
+                            }
+                        }
+                    }
                 }
+                val weeks = remember {
+                    mutableStateListOf(
+                        "一",
+                        "二",
+                        "三",
+                        "四",
+                        "五",
+                        "六",
+                        "日",
+                        "一",
+                        "二",
+                        "三",
+                        "四",
+                        "五",
+                        "六",
+                        "日"
+                    )
+                }
+
+                var itemWidth by remember { mutableStateOf(0.dp) }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    val density = LocalDensity.current
+                    Card(shape = RoundedCornerShape(4.dp)) {
+                        Box(modifier = Modifier
+                            .padding(8.dp)
+                            .wrapContentSize()
+                            .onSizeChanged { intSize ->
+                                itemWidth = with(density) {
+                                    (intSize.width / 7f).toDp()
+                                }
+                            }
+                        ) {
+
+                            LazyRow(
+                                modifier = Modifier.fillMaxWidth(),
+                                content = {
+                                    itemsIndexed(weeks) { index, item ->
+                                        Box(
+                                            Modifier
+                                                .width(itemWidth),
+                                            contentAlignment = Alignment.Center
+                                        ) {
+                                            Text(text = item)
+                                        }
+
+                                    }
+                                })
+                        }
+                    }
+                }
+
             }
         }
     }
--- app/src/main/java/com/eazywrite/app/ui/theme/Theme.kt
+++ app/src/main/java/com/eazywrite/app/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -19,6 +20,7 @@ private val DarkColorScheme = darkColorScheme(
 //    primary = Purple80,
 //    secondary = PurpleGrey80,
 //    tertiary = Pink80
+    onPrimary = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -26,15 +28,14 @@ private val LightColorScheme = lightColorScheme(
 //    secondary = PurpleGrey40,
 //    tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+//     Other default colors to override
+//    background = Color(0xFFFFFBFE),
+//    surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+//    onSecondary = Color.White,
+//    onTertiary = Color.White,
+//    onBackground = Color(0xFF1C1B1F),
+//    onSurface = Color(0xFF1C1B1F),
 )
 
 @Composable
